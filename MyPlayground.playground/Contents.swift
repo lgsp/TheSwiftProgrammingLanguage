066c430a800eeb2036bc2ef322da42f1e575e41a
Laurent Garnier <laurent.garnier.superprof@gmail.com>
2022-04-19 14:54:48 +0200

    Control Flow switch

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,95 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+print(greeting)
+
+var myVariable = 42
+myVariable = 50
+let myConstant = 42
+
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+
+let explicitFloat: Float = 4
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+print("Hello Satoshi, \(0.5) BTC worth a lot now, aren't you happy?")
+
+let quotation = """
+I said "I have \(apples) apples."
+And then I said "I have \(apples + oranges) pieces of fruit."
+"""
+
+var shoppingList = ["catfish", "water", "tulips"]
+shoppingList[1] = "bottle of water"
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+
+occupations["Jayne"] = "Public Relations"
+
+shoppingList.append("blue paint")
+print(shoppingList)
+
+let emptyArray: [String] = []
+let emptyDictionary: [String: Float] = [:]
+
+shoppingList = []
+occupations = [:]
+
+let individualScores = [75, 43, 103, 87, 12]
+
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+
+print(teamScore)
+
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+var optionalName: String? = "Johm Appleseed"
+greeting = "Hello!"
+optionalName = nil
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+print(greeting)
+
+let nickname: String? = nil
+let fullName: String = "Appleseed"
+let informalGreeting = "Hi \(nickname ?? fullName)"
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+default:
+    print("Everything tastes good in soup.")
+}
+
+
+
+
+
